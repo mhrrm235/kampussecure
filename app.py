e69d584b150d034417f1e4161e042e36771a38c7
mhrrm235 <kasdingwanda@gmail.com>
2025-07-30 20:40:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
