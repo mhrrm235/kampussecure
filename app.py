9769e010a4ab457092d239ac380c724d696baa0d
mhrrm235 <kasdingwanda@gmail.com>
2025-07-30 21:11:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,5 +92,4 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Untuk Vercel, tidak perlu app.run()
